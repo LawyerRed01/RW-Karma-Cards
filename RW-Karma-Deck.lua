8f6ee21014ebfa54ece96a3e37abf132c683847e
preston-mattos <lawyerred01@gmail.com>
2025-01-10 17:17:09 -0800

    Added lua file for the mod.

---
+++ RW-Karma-Deck.lua
@@ -0,0 +1,40 @@
+local atlas_key = 'rwKarma'
+local atlas_path = 'RW-Karma-Deck.png'
+local atlas_path_hc = nil
+
+local suits = {'hearts', 'clubs', 'diamonds', 'spades'}
+local ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",}
+
+local description = 'Rain World Karma Deck'
+
+SMODS.Atlas{  
+    key = atlas_key..'_lc',
+    px = 71,
+    py = 95,
+    path = atlas_path,
+    prefix_config = {key = false}, -- See end of file for notes
+}
+
+if atlas_path_hc then
+    SMODS.Atlas{  
+        key = atlas_key..'_hc',
+        px = 71,
+        py = 95,
+        path = atlas_path_hc,
+        prefix_config = {key = false}, -- See end of file for notes
+    }
+end
+
+for _, suit in ipairs(suits) do
+    SMODS.DeckSkin{
+        key = suit.."_skin",
+        suit = suit:gsub("^%l", string.upper),
+        ranks = ranks,
+        lc_atlas = atlas_key..'_lc',
+        hc_atlas = (atlas_path_hc and atlas_key..'_hc') or atlas_key..'_lc',
+        loc_txt = {
+            ['en-us'] = description
+        },
+        posStyle = 'deck'
+    }
+end
