c2f5a2b9f71d2978db4b4e03752e6bd91347cc39
preston-mattos <lawyerred01@gmail.com>
2025-03-06 18:00:47 -0800

    Rewrote the lua file to merge all mod versions into one.

--- RW-Karma-Deck.lua
+++ RW-Karma-Deck.lua
@@ -1,40 +1,111 @@
-local atlas_key = 'rwKarma'
-local atlas_path = 'RW-Karma-Deck.png'
-local atlas_path_hc = 'RW-Karma-Deck-HC.png'
-
 local suits = {'hearts', 'clubs', 'diamonds', 'spades'}
-local ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",}
+local ranks_full = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'}
+local ranks_num = {'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10'}
+local ranks_face = {'Jack', 'Queen', 'King'}
+local display_ranks = {'Ace', 'King', 'Queen', 'Jack', '10'}
 
-local description = 'Rain World'
+-- style_map: construct mapping to atlas for selected style
+--     contrast: 'lc', 'hc' (low contrast, high contrast)
+--     suit_idx: 1-numbered index of suit as it is in atlas (and in suits table)
+--     flavor: 'face-only', 'k1-ace', 'k10-ace'
 
-SMODS.Atlas{  
-    key = atlas_key..'_lc',
-    px = 71,
-    py = 95,
-    path = atlas_path,
-    prefix_config = {key = false},
+function style_map(contrast, suit_idx, flavor)
+    local map = {}
+
+    for idx, rank in ipairs(ranks_full) do
+        map[rank] = {
+            atlas = 'rw-full-'..contrast,
+            pos = {x = idx - 1, y = suit_idx - 1}
+        }
+    end
+
+    if flavor == 'k1-ace' then
+        for idx, rank in ipairs(ranks_num) do
+            map[rank] = {
+                atlas = 'rw-k1ace-'..contrast,
+                pos = {x = idx - 1, y = suit_idx - 1}
+            }
+        end
+    end
+
+    return map
+end
+
+
+-- generate atlases
+local atlas_path = {
+    {'RW-Full-LC.png', 'rw-full-lc'},
+    {'RW-Full-HC.png', 'rw-full-hc'},
+    {'RW-K1-Aces-LC.png', 'rw-k1ace-lc'},
+    {'RW-K1-Aces-HC.png', 'rw-k1ace-hc'}
 }
-
-if atlas_path_hc then
-    SMODS.Atlas{  
-        key = atlas_key..'_hc',
+for _, x in ipairs(atlas_path) do
+    SMODS.Atlas{
+        key = x[2],
         px = 71,
         py = 95,
-        path = atlas_path_hc,
+        path = x[1],
         prefix_config = {key = false},
     }
 end
 
-for _, suit in ipairs(suits) do
+-- generate entries
+for idx, suit in ipairs(suits) do
     SMODS.DeckSkin{
         key = suit.."_skin",
         suit = suit:gsub("^%l", string.upper),
-        ranks = ranks,
-        lc_atlas = atlas_key..'_lc',
-        hc_atlas = (atlas_path_hc and atlas_key..'_hc') or atlas_key..'_lc',
-        loc_txt = {
-            ['en-us'] = description
-        },
-        posStyle = 'deck'
+        loc_txt = { ['en-us'] = "Rain World" },
+        palettes = {{
+                key = 'k10ace-lc',
+                ranks = ranks_full,
+                display_ranks = display_ranks,
+                atlas = 'rw-full-lc',
+                posStyle = style_map('lc', idx, 'k10-ace'),
+                loc_txt = {['en-us'] = "K10 Aces"},
+            },
+            {
+                key = 'k10ace-hc',
+                ranks = ranks_full,
+                display_ranks = display_ranks,
+                atlas = 'rw-full-lc',
+                posStyle = style_map('hc', idx, 'k10-ace'),
+                loc_txt = {['en-us'] = "K10 Aces (High Contrast)"},
+                hc_default = true
+            },
+            {
+                key = 'k1ace-lc',
+                ranks = ranks_full,
+                display_ranks = display_ranks,
+                atlas = 'rw-full-lc',
+                posStyle = style_map('lc', idx, 'k1-ace'),
+                loc_txt = {['en-us'] = "K1 Aces"},
+            },
+            {
+                key = 'k1ace-hc',
+                ranks = ranks_full,
+                display_ranks = display_ranks,
+                atlas = 'rw-full-lc',
+                posStyle = style_map('hc', idx, 'k1-ace'),
+                loc_txt = {['en-us'] = "K1 Aces (High Contrast)"},
+                hc_default = true
+            },
+            {
+                key = 'faceonly-lc',
+                ranks = ranks_face,
+                display_ranks = display_ranks,
+                atlas = 'rw-full-lc',
+                posStyle = style_map('lc', idx, 'face-only'),
+                loc_txt = {['en-us'] = "Face Cards"},
+            },
+            {
+                key = 'faceonly-hc',
+                ranks = ranks_face,
+                display_ranks = display_ranks,
+                atlas = 'rw-full-lc',
+                posStyle = style_map('hc', idx, 'face-only'),
+                loc_txt = {['en-us'] = "Face Cards (High Contrast)"},
+                hc_default = true
+            }
+        }
     }
 end
